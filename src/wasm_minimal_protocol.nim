844e9f33ee1d387ec16f1161e6ee8592e8d34b6a
litlighilit <litlighilit@foxmail.com>
2026-02-06 00:11:33 +0800

    feat(sugar): sup continuous params

--- src/wasm_minimal_protocol.nim
+++ src/wasm_minimal_protocol.nim
@@ -17,8 +17,8 @@ macro export_typst_bytes*(def) =
   var nname = ori_prc_id
   var exportcPragma = ident"exportc"
   let params = def.params
-  let nargsp1 = params.len
-  let nargs = nargsp1 - 1
+  let nIdentDefsp1 = params.len
+  let nIdentDefs = nIdentDefsp1 - 1
   let infResBody = newStmtList()
   let sufResBody = newStmtList()
   let resParams = params.copyNimNode
@@ -27,35 +27,40 @@ macro export_typst_bytes*(def) =
   resParams.add bindSym"Size"  # resType
   let call = newCall ori_prc_id
 
-  let totLensId = genSym(nskVar, "totLens")
-  infResBody.add quote do:
-    var `totLensId`: array[`nargsp1`, `Size`]
-    # an acc sum len
-    # #0 is 0
+  let totLensId = ident("totLens")
 
-  var id: NimNode
-  for i in 1..nargs:  # params[0] is resType
+  var nargs = 0
+  for i in 1..nIdentDefs:  # params[0] is resType
     # a0, a1, ...
     let e = params[i]
-    let eId = e[0]
-    if not (e[2].kind == nnkEmpty or e[2].eqIdent"string"):
-      error "shall be string", e[2]
-    id = ident("a" & $i)
-    resParams.add newIdentDefs(id, bindSym"Size")
-    let ne = ident eId.strVal
-    let i1s = i-1
-    let totLenI1s = quote do: `totLensId`[`i1s`]
-    infResBody.add quote do:
-      `totLensId`[`i`] = `totLenI1s` + `id`
-    call.add ne
-    sufResBody.add quote do:
-      var `ne` = newString(`id`)
-      copyMem(`ne`[0].addr,
-              cast[pointer](cast[int](`bufId`) + `totLenI1s`),
-              `id`
-      )
+    let lastIdx = e.len - 1
+    let lastIdx2 = lastIdx - 1
+    let eType = e[lastIdx2]
+    if not (eType.kind == nnkEmpty or eType.eqIdent"string"):
+      error "only string args allowed for this macro", eType
+    if e[lastIdx].kind != nnkEmpty:
+      error "no default args allowed currently", e[i+1]
+      #TOOD:defval, aware in typst
+    for j in 0..<lastIdx2:
+      nargs += 1
+      let id = genSym(nskParam, "a" & $nargs)
+      resParams.add newIdentDefs(id, bindSym"Size")
+      let ne = ident e[j].strVal
+      let nargs1s = newIntLitNode(nargs - 1)
+      let totLenI1s = quote do: `totLensId`[`nargs1s`]
+      infResBody.add quote do:
+        `totLensId`[`nargs`] = `totLenI1s` + `id`
+      call.add ne
+      sufResBody.add quote do:
+        var `ne` = newString(`id`)
+        copyMem(`ne`[0].addr,
+                cast[pointer](cast[int](`bufId`) + `totLenI1s`),
+                `id`
+        )
   
-  let final = if id.isNil:
+  let resBody = newStmtList()
+  let nargsp1 = nargs + 1
+  let final = if nargs == 0:
     # no arg, cannot overload
     let ori_name = ori_prc_id.strVal
     exportcPragma = nnkExprColonExpr.newTree(
@@ -66,15 +71,20 @@ macro export_typst_bytes*(def) =
     newStmtList()
   else:
     # if has arg
-    let totLen = quote do: `totLensId`[`nargs`]
+    let totLen = quote do: `totLensId`[`nIdentDefs`]
     infResBody.add quote do:
       let `bufId` = alloc(`totLen`)
       wasm_minimal_protocol_write_args_to_buffer(`bufId`)
+    resBody.add quote do:
+      var `totLensId`: array[`nargsp1`, `Size`]
+      # an acc sum len
+      # #0 is 0
     newCall("dealloc", bufId)
 
-  infResBody.add sufResBody
+  resBody.add infResBody
+  resBody.add sufResBody
 
-  infResBody.add quote do:
+  resBody.add quote do:
     try:
       let res = `call`
       send_result(res)
@@ -95,7 +105,7 @@ macro export_typst_bytes*(def) =
     ident"cdecl",
   )
   ndef.add emptyn # reversed
-  ndef.add infResBody
+  ndef.add resBody
   result = newStmtList(
     def,
     ndef
@@ -109,7 +119,7 @@ proc wasm_minimal_protocol_NimMain*: Size{.exportc, cdecl.} =
 
 when isMainModule:
   # mainly to ensure at least macro evaluation works
-  proc f(s: string, s2: string): string{.export_typst_bytes.} =
+  proc f(s, s2: string): string{.export_typst_bytes.} =
     s & '+' & s2 & '$'
 
   proc hello(): string{.export_typst_bytes.} =
