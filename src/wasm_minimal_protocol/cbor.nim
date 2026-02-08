8d8340f55488929ab445d581ec785683a02aeed4
litlighilit <litlighilit@foxmail.com>
2026-02-08 22:14:59 +0800

    feat(cbor): sup char

--- src/wasm_minimal_protocol/cbor.nim
+++ src/wasm_minimal_protocol/cbor.nim
@@ -4,6 +4,11 @@ when defined(cborious):
   import pkg/cborious
   export cborious
   type CborError* = CborInvalidHeaderError
+  template writeValue*(s: Stream, val: auto) = cborPack(s, val)
+  template readValue*(s: Stream, val: var auto) = cborUnpack(s, val)
+  template defineCborPair*(T; writeImpl, readImpl){.dirty.} =
+    proc cborPack*(s: Stream; val: T) = writeImpl
+    proc cborUnpack*(s: Stream; val: var T) = readImpl
   # def is {CborObjToArray, CborCheckHoleyEnums} instead of CborObjToMap
   const flags = {CborObjToMap, CborEnumAsString, CborCheckHoleyEnums}
   template Cbor_encode*[T](d: T): untyped =
@@ -16,6 +21,9 @@ else:
   import pkg/cbor_serialization
   import pkg/cbor_serialization/std/[sets as cbor_sets, tables as cbor_tables]
   export cbor_serialization, cbor_sets, cbor_tables
+  template defineCborPair*(T; writeImpl, readImpl){.dirty.} =
+    proc writeValue*(s: var CborWriter; val: T) = writeImpl
+    proc readValue*(s: var CborReader; val: var T) = readImpl
   template Cbor_encode*[T](d: T): untyped =
     bind Cbor, encode
     encode(typeof Cbor, d)
@@ -23,5 +31,11 @@ else:
     bind Cbor, decode
     decode(typeof Cbor, dd, T)
 
+defineCborPair(char, writeValue(s, $val)):
+  var v: string
+  readValue(s, v)
+  if v.len != 1:
+    raise newException(CborError, "Expected a single-character string for char unpacking")
+  val = v[0]
 
 
